7a27b1b239d50b65efda30ca9e923b393a6bc599
Krupali Rao <65597425+krupali2507@users.noreply.github.com>
2021-10-29 16:12:38 +0530

    Instructions added

--- simple_programs/faulty_calculator.py
+++ simple_programs/faulty_calculator.py
@@ -1,3 +1,12 @@
+#Faulty Calculator
+# 45 * 3 = 555, 56+9 = 77, 56/6 = 4
+# Design a calculator which will correctly solve all the problems except
+# the following ones:
+# 45 * 3 = 555, 56+9 = 77, 56/6 = 4
+# Your program should take operator  and the two numbers as input from the user
+# and then return the result
+
+
 operation = input(" + for Addition \n - for Substraction \n * for multiplication \n / for Division \n Enter input: ");
 
 #45 * 3 = 555 , 56 + 9 = 77 , 56/6 = 4
